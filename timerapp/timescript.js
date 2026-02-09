2a59b63ceec951407ee4a10bf54ed828f0bc0a9e
Kacper <kacperkawec481@gmail.com>
2026-02-09 17:54:59 +0000

    dd

--- timerapp/timescript.js
+++ timerapp/timescript.js
@@ -4,4 +4,5 @@ function timersetup(){
     let space = " "
     let specialevent = "None";
     let monthyear = month + space + year
+    console.log(monthyear) //change later to document.getElementById
     }
