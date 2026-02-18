b83eea24c00adc4bc2d157ed8671cad381d5b1ee
Kacper <kacperkawec481@gmail.com>
2026-02-18 11:08:03 +0000

    f

--- timerapp/timescript.js
+++ timerapp/timescript.js
@@ -3,7 +3,7 @@ function timersetup(){
     let year = "2026";
     let space = " "
     let specialevent = "4 days after Valentine's";
-    let monthyear = month + space + year;
+    const monthyear = month + space + year;
     let fovvv = 1
     while (fovvv === 1){
         document.getElementById("time1").innerHTML = monthyear;
