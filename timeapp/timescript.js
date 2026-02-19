c2f8b268fd005f3c9df9e7e470bfa3ef17bfe3c0
Kacper <kacperkawec481@gmail.com>
2026-02-19 16:50:35 +0000

    add seconds counter and new icon to website - 0.2

--- timeapp/timescript.js
+++ timeapp/timescript.js
@@ -1,9 +1,15 @@
+let secss = 0
 function timersetup(){
     let month = "February"
     let year = "2026";
     let space = " "
-    let specialevent = "4 days after Valentine's";
+    let specialevent = "Valentine's has happened recently";
     const monthyear = month + space + year;
         document.getElementById("time1").innerHTML = monthyear;
     document.getElementById("specialthing").innerHTML = specialevent;
+    let ff = 1;
+    while (ff===1){
+secss = secss + 1;
+document.getElementById("seconds").innerHTML = secss;
+    };
     }
