5b427ebd8d3b58ac1434f8e418a139f9a284e3c5
Kacper <kacperkawec481@gmail.com>
2026-02-23 16:34:44 +0000

    MORE bug fixes

--- plumclicker/scripts.js
+++ plumclicker/scripts.js
@@ -1,16 +1,15 @@
 let clickss = 0    
 let timeusedorwasted = 0
 let idk = 1
-function addtheclicks(){  clickss = clickss + 1;
+function addtheclicks(){  
+    clickss = clickss + 1;    
+    document.getElementById('pointsdisplay').innerHTML = clickss;   
   if (clickss > 29){
         alert('wow you actually spent your time doing this');
         alert('screenshot this to show off to your friends for absolutley no reason');
         console.log(idk);
         console.log('wow the player did this')
   }
-        setInterval(function otherthing(){
-    document.getElementById('pointsdisplay').innerHTML = clickss;   
-    }, 500);
 setInterval(function otherotherthing(){
 timeusedorwasted = timeusedorwasted + 1;
 }, 1000)  
