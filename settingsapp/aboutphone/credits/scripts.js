de8624169e31e6ad2db547a8049c7665b5965d5c
Kacper <kacperkawec481@gmail.com>
2026-03-02 16:13:10 +0000

    more scripts

--- settingsapp/aboutphone/credits/scripts.js
+++ settingsapp/aboutphone/credits/scripts.js
@@ -3,7 +3,14 @@ function devtools(){
 if (devtools === 1){
     let choice = prompt("a - crazy mode");
     if (choice === "a"){
+        let thing = prompt('purple (a) or weird (b)?')
+        if (thing === "a"){
         document.getElementById("hi").style.backgroundColor = "purple";
-    }
-}}
+      }    
+      if (thing = "b"){
+        document.getElementById("hi").style.backgroundColor = "honeydew"
+      }
+     }
+  }
+}
 
