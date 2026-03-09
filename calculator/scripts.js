046f14cc4e1a7254fa059a59366fcc5f902ea9d3
Kacper <kacperkawec481@gmail.com>
2026-03-09 16:29:16 +0000

    add calculator app

---
+++ calculator/scripts.js
@@ -0,0 +1,19 @@
+function choosenumcalc(){
+    let num1 = prompt('num1?');
+     let ope = prompt('operators');
+     let num2 = prompt('f');
+     let ans = 0;
+     if (ope === "*"){
+ans = num1 * num2;
+     }
+     if (ope === "+"){
+          ans = num1 + num2;
+     }
+     if (ope === "/"){
+          ans = num1/num2;
+     }
+     if (ope === "-"){
+          ans = num1-num2;
+     }
+     document.getElementById("answer").innerHTML = ans;
+}
