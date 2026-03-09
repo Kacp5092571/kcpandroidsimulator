7eda3515b601d3aa98e67861cb5453d07d958ec7
Kacper <kacperkawec481@gmail.com>
2026-03-09 16:38:31 +0000

    fixxx

--- calculator/scripts.js
+++ calculator/scripts.js
@@ -1,8 +1,10 @@
 function choosenumcalc(){
     let num1 = prompt('num1?');
      let ope = prompt('operators');
-     let num2 = prompt('f');
+     let num2 = prompt('num2');
      let ans = 0;
+     parseInt(num1)
+     parseInt(num2)
      if (ope === "*"){
 ans = num1 * num2;
      }
@@ -15,6 +17,7 @@ ans = num1 * num2;
      if (ope === "-"){
           ans = num1-num2;
      }
+     parseInt(ans)
      prompt(ans)
      document.getElementById("answer").innerHTML = ans;
 }
